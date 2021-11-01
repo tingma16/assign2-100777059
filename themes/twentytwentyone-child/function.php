83a5a8b6e22bb054d8165341ccad8c984124f732
ting.ma <tingma16@gmail.com>
2021-11-01 18:45:24 -0400

    menu and child theme
    add menu and child theme

---
+++ themes/twentytwentyone-child/function.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+function twentytwentyonechild_wp_enqueue_scripts() {
+    $parenthandle = 'twenty-twenty-one-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( 
+        $parenthandle, 
+        get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 
+        'twentytwentyonechild-style', 
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'twentytwentyonechild_wp_enqueue_scripts' );
+
+?>
